bf15501eb3476db6f71947426c4c993997d89261
WillNetsky <willnetsky@gmail.com>
2016-06-23 17:37:47 -0700

    similarity keywords added, doesnt look good yet

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -13,6 +13,19 @@ lsi = pickle.load(open('app/models/lsi.pkl','rb'))
 corpus = pickle.load(open('app/models/corpus.pkl','rb'))
 beers = pd.read_pickle('app/models/beer_review_df.pkl')
 indexx = pickle.load(open('app/models/index.pkl','rb'))
+corpus_tfidf = pickle.load(open('app/models/tfidf.pkl','rb'))
+
+def get_beer_keywords(text_input, similar_beers):
+	input_beer_keywords = []
+	for item in sorted(corpus_tfidf[beers[beers.name == text_input].index[0]], key = lambda x: -x[1])[:5]:
+		input_beer_keywords.append(dictionary[item[0]])
+
+	similar_beer_words = []
+	for beer in list(similar_beers.index):
+	    similar_beer_words.append([dictionary[item[0]] for
+	    	item in sorted(corpus_tfidf[beer], key = lambda x: -x[1])[:5]
+	    	if dictionary[item[0]] in input_beer_keywords])
+	return (input_beer_keywords, similar_beer_words)
 
 def get_beer_names():
 	return list(beers.name)
@@ -38,7 +51,7 @@ def get_recs_from_input(text_input):
     for beer in sorted(enumerate(sims), key = lambda x: -x[1])[beer_name_inputted:beer_name_inputted+5]:
         print(beers.name[beer[0]] + ' : %.2f' % (beer[1]*100))
         similar_beers.append(beer[0])
-    similar_beers = similar_beers_formatting(beers.iloc[similar_beers])
+    similar_beers = beers.iloc[similar_beers]
     return (similar_beers,beer_name_inputted)
 
 from .views import *
--- flask/app/views.py
+++ flask/app/views.py
@@ -9,7 +9,7 @@ from wtforms.validators import Required
 import pandas as pd
 import json
 
-from . import app, get_recs_from_input, get_beer_names, get_brewery_names
+from . import app, get_recs_from_input, get_beer_names, get_brewery_names, get_beer_keywords, similar_beers_formatting
 
 
 beer_names = get_beer_names()
@@ -31,7 +31,10 @@ def index():
 
     """Index page"""
     form = PredictForm()
-    prediction = (None, None)
+
+    similar_beers = None
+    beer_keywords = (None, None)
+    beer_inputted = False
 
     if form.validate_on_submit():
         # store the submitted values
@@ -42,13 +45,20 @@ def index():
         beer_input = submitted_data['beer_input']
 
         # Get similar beer recommendations
-        prediction = get_recs_from_input(beer_input)
+        similar_beers = get_recs_from_input(beer_input)
+        beer_inputted = similar_beers[1]
+        
+        beer_keywords = get_beer_keywords(beer_input,similar_beers[0])
+        similar_beers = similar_beers_formatting(similar_beers[0])
+        input_keywords = beer_keywords[0]
+        similar_keywords = beer_keywords[1]
 
-    return render_template('index.html', form=form, beer_inputted = prediction[1], similar_beers=prediction[0])
+
+    return render_template('index.html', form=form, beer_inputted = beer_inputted, similar_beers=similar_beers, input_keywords = beer_keywords[0], similar_keywords = beer_keywords[1])
 
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
     search = request.args.get('term')
     app.logger.debug(search)
 
-    return jsonify(beer=beer_names,brewery = brewery_names)
+    return jsonify(beer=beer_names, brewery = brewery_names)
